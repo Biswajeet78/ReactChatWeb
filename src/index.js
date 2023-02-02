9e79a30f1aaf38226dd7fe166ca2971ba685812b
Biswajeet Sahoo <97235820+Biswajeet78@users.noreply.github.com>
2023-02-02 17:09:51 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './components/App';
+import App from './App';
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5WDLaDNDXlT4vbkNiq0-PzjMbkwZ9pww",
+  authDomain: "react-chat-web-friends.firebaseapp.com",
+  projectId: "react-chat-web-friends",
+  storageBucket: "react-chat-web-friends.appspot.com",
+  messagingSenderId: "520000337696",
+  appId: "1:520000337696:web:f0ab95d74b210aba9ac945"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<BrowserRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-</BrowserRouter>
-)
+  <App />
+);
